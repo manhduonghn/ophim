da7c442388863b8d06675ed85a80595dccda20e6
manhduonghn <manhduonghn@live.com>
2026-03-23 21:43:22 +0700

    nâng cấp nullable

--- app/src/main/java/com/example/ophim/model/Movie.kt
+++ app/src/main/java/com/example/ophim/model/Movie.kt
@@ -1,13 +1,13 @@
 package com.example.ophim.model
 
 data class Movie(
-    val _id: String,
+    val _id: String?,
     val name: String,
     val slug: String,
-    val origin_name: String,
+    val origin_name: String?,
     val thumb_url: String,
     val poster_url: String?,
-    val year: Int,
+    val year: Int?,
     val lang: String?,
     val quality: String?,
     val episode_current: String?
