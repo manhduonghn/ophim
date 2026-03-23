8702ebb0ca2fb72e17a16cbf6b34099d2708cf71
manhduonghn <manhduonghn@live.com>
2026-03-23 12:01:17 +0700

    Thêm thông tin phim

--- app/src/main/java/com/example/ophim/model/DetailResponse.kt
+++ app/src/main/java/com/example/ophim/model/DetailResponse.kt
@@ -1,14 +1,30 @@
 package com.example.ophim.model
 
-data class DetailResponse(val status: String, val data: DetailData)
-data class DetailData(val item: MovieDetail)
+data class DetailResponse(
+    val status: String,
+    val data: DetailData
+)
+
+data class DetailData(
+    val item: MovieDetail
+)
+
 data class MovieDetail(
-    val name: String, 
-    val content: String, 
-    val poster_url: String, 
+    val name: String,
+    val content: String,
+    val poster_url: String,
+    val year: Int?,
+    val lang: String?,
+    val quality: String?,
+    val time: String?,
+
     val episodes: List<Server>
 )
-data class Server(val server_name: String, val server_data: List<Episode>)
+
+data class Server(
+    val server_name: String,
+    val server_data: List<Episode>
+)
 
 data class Episode(
     val name: String,
