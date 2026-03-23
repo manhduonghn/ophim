9a2e8334228d46aa429b509f1e5d59e6e3a62728
manhduonghn <manhduonghn@live.com>
2026-03-23 09:12:30 +0700

    Create DetailResponse.kt

---
+++ app/src/main/java/com/example/ophim/model/DetailResponse.kt
@@ -0,0 +1,17 @@
+package com.example.ophim.model
+
+data class DetailResponse(val status: String, val data: DetailData)
+data class DetailData(val item: MovieDetail)
+data class MovieDetail(
+    val name: String, 
+    val content: String, 
+    val poster_url: String, 
+    val episodes: List<Server>
+)
+data class Server(val server_name: String, val server_data: List<Episode>)
+
+data class Episode(
+    val name: String,
+    val slug: String,
+    val link_m3u8: String
+)
