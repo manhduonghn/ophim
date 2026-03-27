9b651508f955d2b2a40b3941c89be8fee6d9a256
manhduonghn <manhduonghn@live.com>
2026-03-27 07:51:48 +0700

    Search api

--- app/src/main/java/com/example/ophim/api/ApiService.kt
+++ app/src/main/java/com/example/ophim/api/ApiService.kt
@@ -4,13 +4,18 @@ import com.example.ophim.model.*
 import retrofit2.http.*
 
 interface ApiService {
+    // Lấy dữ liệu trang chủ (Phim mới cập nhật)
+    @GET("v1/api/home")  
+    suspend fun getHome(): HomeResponse  
 
-    @GET("v1/api/home")
-    suspend fun getHome(): HomeResponse
-
+    // Lấy dữ liệu tìm kiếm theo từ khóa
     @GET("v1/api/tim-kiem")
-    suspend fun search(@Query("keyword") keyword: String): HomeResponse
-
-    @GET("v1/api/phim/{slug}")
+    suspend fun search(
+        @Query("keyword") query: String,
+        @Query("page") page: Int
+    ): SearchResponse
+    
+    // Lấy chi tiết một bộ phim
+    @GET("v1/api/phim/{slug}")  
     suspend fun getDetail(@Path("slug") slug: String): DetailResponse
 }
