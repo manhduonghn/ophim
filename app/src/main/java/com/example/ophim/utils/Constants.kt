48d6ca9c6823efd39e5e4b85acf95216bc4db3f1
manhduonghn <manhduonghn@live.com>
2026-03-28 08:10:13 +0700

    Create Constants

---
+++ app/src/main/java/com/example/ophim/utils/Constants.kt
@@ -0,0 +1,13 @@
+package com.example.ophim.utils
+
+object Constants {
+    val MOVIE_TYPES = mapOf(
+        "Phim Bộ" to "v1/api/danh-sach/phim-bo",
+        "Phim Lẻ" to "v1/api/danh-sach/phim-le",
+        "Hoạt Hình" to "v1/api/danh-sach/hoat-hinh",
+        "TV Shows" to "v1/api/danh-sach/tv-shows",
+        "Phim Vietsub" to "v1/api/danh-sach/phim-vietsub",
+        "Phim Thuyết Minh" to "v1/api/danh-sach/phim-thuyet-minh",
+        "Phim Chiếu Rạp" to "v1/api/danh-sach/phim-chieu-rap"
+    )
+}
